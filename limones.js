9ed0c0818b4aebabc9881ad0d3c49bd2ce74deb0
hgxAri <boahanc9ck.com>
2026-07-08 22:14:11 -0500

    agg dibujar suelo y dibujar personaje

--- limones.js
+++ limones.js
@@ -1,5 +1,18 @@
 let canvas = document.getElementById('areajuego');
 let ctx = canvas.getContext('2d');
+function iniciarJuego() {
+    dibujarsuelo();
+    dibujarJugador();
+}
+function dibujarsuelo() {
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, canvas.height - 20, canvas.width, 20);
+}
+function dibujarJugador() {
+    jugador.x = Math.max(0, Math.min(canvas.width - jugador.width, jugador.x));
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(canvas.width  / 2, canvas.height - (20+60), 40, 60);
+}
 let puntaje = 0;
 let limones = [];
 let jugador = {
