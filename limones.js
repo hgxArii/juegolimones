6823473584d3f3b080703bc663da9e7acba7b72b
hgxAri <boahanc9ck.com>
2026-07-12 17:21:36 -0500

    mover izq y der

--- limones.js
+++ limones.js
@@ -1,19 +1,35 @@
-let canvas = document.getElementById('areajuego');
-let ctx = canvas.getContext('2d');
-const ALTURASUELO = 40;
+let canvas = document.getElementById("areajuego");
+let ctx = canvas.getContext("2d");
+const ALTURASUELO = 20;
 const ALTURAPERSONAJE = 60;
 const ANCHOPERSONAJE = 40;
+let personajeX=canvas.width/2;
 function iniciarJuego() {
     dibujarsuelo();
     dibujarJugador();
 }
 function dibujarsuelo() {
-    ctx.fillStyle = 'green';
-    ctx.fillRect(0, canvas.height - ALTURASUELO, canvas.width, 20);
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, canvas.height - ALTURASUELO, canvas.width, ALTURASUELO);
 }
 function dibujarJugador() {
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(canvas.width  / 2, canvas.height - (ALTURASUELO+ALTURAPERSONAJE), ANCHOPERSONAJE, ALTURAPERSONAJE);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(personajeX, canvas.height - (ALTURASUELO+ALTURAPERSONAJE), ANCHOPERSONAJE, ALTURAPERSONAJE);
+}
+function moverIzquierda() {
+    personajeX=personajeX-10;
+    limpiarCanvas();
+    dibujarJugador();
+    }
+function limpiarCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    dibujarsuelo();
+    dibujarJugador();
+}
+function moverDerecha() {
+    personajeX=personajeX+10;
+    limpiarCanvas();
+    dibujarJugador();
 }
 let puntaje = 0;
 let limones = [];
