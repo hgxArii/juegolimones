83e0cdeafd0a0d50d99a8945fe9fb6d141f124ca
hgxAri <boahanc9ck.com>
2026-07-21 00:23:17 -0500

    agg limon

--- limones.js
+++ limones.js
@@ -3,10 +3,15 @@ let ctx = canvas.getContext("2d");
 const ALTURASUELO = 20;
 const ALTURAPERSONAJE = 60;
 const ANCHOPERSONAJE = 40;
+const ANCHOLIMON = 20;
+const ALTOLIMON = 20;
 let personajeX=canvas.width/2;
+let limonX=canvas.width/2;
+let limonY=5;
 function iniciarJuego() {
     dibujarsuelo();
     dibujarJugador();
+    dibujarLimon();
 }
 function dibujarsuelo() {
     ctx.fillStyle = "green";
@@ -19,18 +24,32 @@ function dibujarJugador() {
 function moverIzquierda() {
     personajeX=personajeX-10;
     limpiarCanvas();
-    dibujarJugador();
+    actualizarPantalla();
     }
-function limpiarCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    dibujarsuelo();
-    dibujarJugador();
-}
 function moverDerecha() {
     personajeX=personajeX+10;
     limpiarCanvas();
-    dibujarJugador();
+    actualizarPantalla();
 }
+function actualizarPantalla() {
+    limpiarCanvas();
+    dibujarJugador();
+    dibujarsuelo();
+    dibujarLimon();
+}
+function limpiarCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+function dibujarLimon() {
+        ctx.fillStyle = "yellow";
+        ctx.fillRect(limonX,limonY,ANCHOLIMON,ALTOLIMON);
+       
+}
+function bajarlimon() {
+    limonY = limonY + 10;
+    dibujarLimon();
+    actualizarPantalla();
+ }   
 let puntaje = 0;
 let limones = [];
 let jugador = {
