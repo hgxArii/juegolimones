85670eece00db809e28d0e96d0489582886be551
hgxAri <boahanc9ck.com>
2026-07-21 19:45:19 -0500

    agg colisiones

--- limones.js
+++ limones.js
@@ -6,34 +6,37 @@ const ANCHOPERSONAJE = 40;
 const ANCHOLIMON = 20;
 const ALTOLIMON = 20;
 let personajeX=canvas.width/2;
+let personajeY=canvas.height-(ALTURASUELO + ALTURAPERSONAJE);
 let limonX=canvas.width/2;
 let limonY=5;
 function iniciarJuego() {
     dibujarsuelo();
-    dibujarJugador();
+    dibujarPersonaje();
     dibujarLimon();
 }
 function dibujarsuelo() {
     ctx.fillStyle = "green";
     ctx.fillRect(0, canvas.height - ALTURASUELO, canvas.width, ALTURASUELO);
 }
-function dibujarJugador() {
+function dibujarPersonaje() {
     ctx.fillStyle = "blue";
-    ctx.fillRect(personajeX, canvas.height - (ALTURASUELO+ALTURAPERSONAJE), ANCHOPERSONAJE, ALTURAPERSONAJE);
+    ctx.fillRect(personajeX, personajeY, ANCHOPERSONAJE, ALTURAPERSONAJE);
 }
 function moverIzquierda() {
     personajeX=personajeX-10;
     limpiarCanvas();
     actualizarPantalla();
+    detectarColision();
     }
 function moverDerecha() {
     personajeX=personajeX+10;
     limpiarCanvas();
     actualizarPantalla();
+    detectarColision();
 }
 function actualizarPantalla() {
     limpiarCanvas();
-    dibujarJugador();
+    dibujarPersonaje();
     dibujarsuelo();
     dibujarLimon();
 }
@@ -50,6 +53,18 @@ function bajarlimon() {
     dibujarLimon();
     actualizarPantalla();
  }   
+ function detectarColision() {
+    if (limonY == personajeX && limonY == personajeY)
+    {
+     alert("¡Has atrapado el limón!");   
+    }
+    }
+function detectarColision() {
+    if (limonY + ALTOLIMON >= personajeY && limonX + ANCHOLIMON >= personajeX && limonX <= personajeX + ANCHOPERSONAJE) {
+        alert("¡Has atrapado el limón!");
+        reiniciarJuego();
+    }
+     }
 let puntaje = 0;
 let limones = [];
 let jugador = {
